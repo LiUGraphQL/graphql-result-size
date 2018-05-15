ca3d8e0361a6952d8b9b26ecba259884079a1a8b
timandersson <andersson.tim91@gmail.com>
2018-05-15 13:26:46 +0200

    Calc function promise

--- calculate.js
+++ calculate.js
@@ -1,8 +1,22 @@
 var _ = require('lodash');
 
 
-const queryCalculator = ()  => {
-  console.log("test");
+const queryCalculator = (g, maxSize, validationContext) => {
+  try {
+    if (_.size(validationContext.getDocument().definitions) > 1) {
+      return Promise.resolve(validationContext);
+    }
+
+    return Promise.resolve().then(() => {
+      return validationContext;
+    });
+
+  } catch (err) {
+    {
+      console.error(err);
+      throw err;
+    }
+  }
 };
 
 module.exports = queryCalculator;
